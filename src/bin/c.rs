ca5cadbad82d89225b3cde50f515746dc55c2fed
鎌田正聖 <masatoshi@masa-macbook-pro.local>
2020-07-17 23:29:18 +0900

    Done c

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -4,5 +4,19 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        mut k: u128,
+        a: u128,
+        b: u128
+    }
+    if b - a <= 2 {
+        println!("{}", 1 + k);
+    } else {
+        let mut biscuits = 1;
+        biscuits += a - 1;
+        k -= a - 1;
+        biscuits += k / 2 * (b - a);
+        k = k % 2;
+        biscuits += k;
+        println!("{}", biscuits);
     }
 }
